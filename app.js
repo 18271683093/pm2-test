50117c608625c26ff0cc94eb1ae95a3f08b614b4
unknown <18271683093>
2019-12-30 14:36:08 +0800

    dsd

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const homePage = `
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style type="text/css">
-    
         * {
             padding: 0;
             margin: 0;
